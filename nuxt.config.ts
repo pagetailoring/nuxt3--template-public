3e4cf45636e6de28e939a992679f1962ec71b784
Page Tailoring <bartek@pagetailoring.com>
2024-10-30 10:54:45 +0100

    basic css styles & meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['~/assets/styles/global.scss'],
 
   modules: [
     '@nuxt/eslint',
@@ -17,5 +18,22 @@ export default defineNuxtConfig({
   site: {
     url: process.env.DOMAIN,
     trailingSlash: true
+  },
+
+  app: {
+    // https://nuxt.com/docs/getting-started/transitions#page-transitions
+    pageTransition: { name: 'page', mode: 'out-in' },
+
+    // https://nuxt.com/docs/api/nuxt-config#head
+    head: {
+      htmlAttrs: { lang: 'pl' },
+      title: process.env.BRAND,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
+        { name: 'description', content: process.env.DESCRIPTION }
+      ],
+      link: [{ rel: 'canonical', href: process.env.DOMAIN }]
+    }
   }
 })
